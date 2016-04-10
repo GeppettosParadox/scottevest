07f865f88ca62482826cc2858b8d8b94816897fb
Patrick Harrington <patrick@mildlygeeky.com>
2016-04-10 15:00:58 -0400

    Adds good bit of xray to homepage

---
+++ public/scripts/main.js
@@ -0,0 +1,6 @@
+$(window).load(function(){
+  $(".xray-img").twentytwenty({
+    default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+    orientation: 'horizontal' // Orientation of the before and after images ('horizontal' or 'vertical')
+  });
+});
