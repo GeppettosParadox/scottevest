45c042e07ba3bf439a6a4244185fc544cca1ae88
Patrick Harrington <patrick@mildlygeeky.com>
2016-04-10 21:11:56 -0400

    Completes most of xray, good bit of explore products grid

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,27 @@
+$(document).ready(function () {
+    $('.product-grid__filters button').on('click', function() {
+        var $button = $(this);
+        var $category = $(this).attr('data-productcat');
+        var $products = $button.closest('.product-grid').find('.product-grid__product');
+        $button
+            .siblings()
+            .addBack()
+            .removeClass('active')
+        ;
+        $button
+            .addClass('active')
+        ;
+        $products.addClass('hide');
+        if ($category !== '') {
+            $products.filter('[data-productcat="' + $category + '"]')
+                .removeClass('hide')
+            ;
+        } else {
+            $products.removeClass('hide');
+        }
+    });
+});
+
 $(window).load(function(){
   $(".xray-img").twentytwenty({
     default_offset_pct: 0.5, // How much of the before image is visible when the page loads
